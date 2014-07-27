48d57e1d94c874738f5947dfdaf2cda42aea20ae
Constantin Schomburg <me@cschomburg.com>
2014-07-27 22:00:08 +0200

    proto/client: connect on first message, start goroutines on every handler

--- proto/client/client.go
+++ proto/client/client.go
@@ -11,6 +11,7 @@ type Client struct {
 	DeviceId      string
 	subscriptions []subscription
 	transport     Transport
+	connected     bool
 }
 
 type Transport interface {
@@ -30,6 +31,7 @@ func New(deviceName string) *Client {
 		deviceName + "-" + proto.GenerateId(),
 		make([]subscription, 0),
 		nil,
+		false,
 	}
 	return c
 }
@@ -42,6 +44,7 @@ func (c *Client) Connect() error {
 	if err := c.transport.Connect(c.DeviceId, c.handleMessage); err != nil {
 		return err
 	}
+	c.connected = true
 
 	if err := c.Subscribe("ping", c.handlePing); err != nil {
 		return err
@@ -69,6 +72,12 @@ func (c *Client) FillMessage(msg *proto.Message) {
 }
 
 func (c *Client) Publish(msg proto.Message) error {
+	if !c.connected {
+		err := c.Connect()
+		if err != nil {
+			return err
+		}
+	}
 	c.FillMessage(&msg)
 	return c.transport.Publish(msg)
 }
@@ -76,7 +85,7 @@ func (c *Client) Publish(msg proto.Message) error {
 func (c *Client) handleMessage(msg proto.Message) {
 	for _, sub := range c.subscriptions {
 		if strings.HasPrefix(msg.Action+"/", sub.Action+"/") {
-			sub.Handler(msg)
+			go sub.Handler(msg)
 		}
 	}
 }
@@ -91,6 +100,12 @@ func (c *Client) handlePing(msg proto.Message) {
 }
 
 func (c *Client) Subscribe(action string, handler proto.MessageHandler) error {
+	if !c.connected {
+		err := c.Connect()
+		if err != nil {
+			return err
+		}
+	}
 	log.Default.Debugf("[client] subscribing to '%s'", action)
 	c.subscriptions = append(c.subscriptions, subscription{action, handler})
 	return c.transport.Subscribe(action, "", "")
