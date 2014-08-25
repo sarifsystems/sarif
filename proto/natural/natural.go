52c39d7cc37b9363b6a8ea22a521fa7190c8757a
Constantin Schomburg <me@cschomburg.com>
2014-08-25 21:54:55 +0200

    add foundation for natural language processing

---
+++ proto/natural/natural.go
@@ -0,0 +1,45 @@
+package natural
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/xconstruct/stark/proto"
+)
+
+func ParseSimple(text string) (proto.Message, bool) {
+	msg := proto.Message{}
+	parts := strings.Split(text, " ")
+	msg.Action = parts[0]
+	if msg.Action == "" {
+		return msg, false
+	}
+
+	payload := make(map[string]interface{}, 0)
+	for _, part := range parts[1:] {
+		keyval := strings.SplitN(part, "=", 2)
+		if len(keyval) == 1 {
+			return msg, false
+		}
+
+		k, v := keyval[0], keyval[1]
+		switch k {
+		case "device":
+			msg.Device = v
+		default:
+			payload[k] = v
+		}
+	}
+	if len(payload) > 0 {
+		msg.Payload = payload
+	}
+	return msg, true
+}
+
+func FormatSimple(msg proto.Message) string {
+	if text := msg.PayloadGetString("text"); text != "" {
+		return text
+	}
+
+	return fmt.Sprintf("%s from %s.", msg.Action, msg.Source)
+}
---
+++ proto/natural/natural_test.go
@@ -0,0 +1,47 @@
+package natural
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/xconstruct/stark/proto"
+)
+
+type simple struct {
+	text     string
+	ok       bool
+	expected proto.Message
+}
+
+func TestParseSimple(t *testing.T) {
+	tests := []simple{
+		simple{"ping", true, proto.Message{
+			Action: "ping",
+		}},
+
+		simple{"ping device=me", true, proto.Message{
+			Action: "ping",
+			Device: "me",
+		}},
+
+		simple{"ping device=me host=another", true, proto.Message{
+			Action: "ping",
+			Device: "me",
+			Payload: map[string]interface{}{
+				"host": "another",
+			},
+		}},
+
+		simple{"ping no", false, proto.Message{}},
+		simple{"", false, proto.Message{}},
+	}
+
+	for _, test := range tests {
+		msg, ok := ParseSimple(test.text)
+		if ok != test.ok {
+			t.Errorf("'%s' should parse? exp: %v, got: %v", test.text, test.ok, ok)
+		} else if ok && !reflect.DeepEqual(msg, test.expected) {
+			t.Errorf("decoded message differs\nexp '%v'\ngot '%v'", test.expected, msg)
+		}
+	}
+}
