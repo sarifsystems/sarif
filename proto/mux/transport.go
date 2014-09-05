07e498aa87f9b209392474436ca8ee8ceb35afe9
Constantin Schomburg <me@cschomburg.com>
2014-09-05 17:09:48 +0200

    decouple TransportMux from Mux and in turn solve the duplicate message bug

--- proto/mux/transport.go
+++ proto/mux/transport.go
@@ -10,25 +10,22 @@ import "github.com/xconstruct/stark/proto"
 type TransportMux struct {
 	publisher proto.Publisher
 	endpoints []*Endpoint
-	*Mux
 }
 
 type Endpoint struct {
 	mux     *TransportMux
 	handler proto.Handler
+	subs    []Subscription
 }
 
 func (e *Endpoint) Publish(msg proto.Message) error {
 	if msg.Action == "proto/sub" {
-		action := msg.PayloadGetString("action")
-		device := msg.PayloadGetString("device")
-		e.mux.RegisterHandler(action, device, e.handle)
+		e.subs = append(e.subs, Subscription{
+			Action: msg.PayloadGetString("action"),
+			Device: msg.PayloadGetString("device"),
+		})
 	}
 	return e.mux.publisher(msg)
-}
-
-func (e *Endpoint) handle(msg proto.Message) {
-	e.handler(msg)
 }
 
 func (e *Endpoint) RegisterHandler(h proto.Handler) {
@@ -39,10 +36,22 @@ func NewTransportMux() *TransportMux {
 	m := &TransportMux{
 		nil,
 		make([]*Endpoint, 0),
-		New(),
 	}
-	m.Mux.concurrent = true
 	return m
+}
+
+func (m *TransportMux) Handle(msg proto.Message) {
+	for _, e := range m.endpoints {
+		if e.handler == nil {
+			continue
+		}
+		for _, s := range e.subs {
+			if s.Matches(msg) {
+				e.handler(msg)
+				break
+			}
+		}
+	}
 }
 
 func (m *TransportMux) RegisterPublisher(p proto.Publisher) {
@@ -50,7 +59,7 @@ func (m *TransportMux) RegisterPublisher(p proto.Publisher) {
 }
 
 func (m *TransportMux) NewEndpoint() *Endpoint {
-	e := &Endpoint{m, nil}
+	e := &Endpoint{m, nil, make([]Subscription, 0)}
 	m.endpoints = append(m.endpoints, e)
 	return e
 }
