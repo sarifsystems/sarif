279809af36d5ffe1104caa3f61ba9eaf26f0f8ce
Constantin Schomburg <me@cschomburg.com>
2015-02-22 13:44:12 +0100

    Web: generate assets.

---
+++ assets/web/web.go
@@ -0,0 +1,8 @@
+// Copyright (C) 2014 Constantin Schomburg <me@cschomburg.com>
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
+//go:generate go-bindata -pkg web -ignore=.go$ -o assets_gen.go ./...
+
+package web
