fc0c8228a58f80fe82dbb849d3b507bb8281a57f
Constantin Schomburg <me@cschomburg.com>
2015-06-24 17:37:18 +0200

    Web: WIP stats page.

--- assets/web/js/app.js
+++ assets/web/js/app.js
@@ -38,6 +38,10 @@ mainApp.config(function($routeProvider, $locationProvider) {
 		.when('/daily/:date?', {
 			templateUrl: 'pages/daily.html',
 			controller: 'DailyCtrl'
+		})
+		.when('/stats', {
+			templateUrl: 'pages/stats.html',
+			controller: 'StatsCtrl'
 		});
 	$locationProvider.html5Mode(false);
 });
@@ -195,4 +199,91 @@ mainApp.controller('DailyCtrl', function($scope, $location, $routeParams, stark)
 		date = date.substring(0, date.indexOf('T'));
 		$location.path('/daily/' + date);
 	};
+});
+
+mainApp.controller('StatsCtrl', function($scope, $location, $routeParams, stark) {
+	$scope.dateStart= new Date();
+	if ($routeParams.date) {
+		$scope.dateStart = new Date($routeParams.date);
+	}
+	$scope.dateStart.setHours(4, 0, 0, 0);
+	$scope.dateEnd = new Date($scope.dateStart.getTime());
+	$scope.dateEnd.setHours(28);
+
+	$scope.changeDay = function(addDays) {
+		var date = new Date($scope.dateStart.getTime());
+		date.setDate(date.getDate() + addDays);
+		date = date.toISOString();
+		date = date.substring(0, date.indexOf('T'));
+		$location.path('/daily/' + date);
+	};
+
+	stark.Request({
+		action: 'event/list',
+		p: {
+			action: "devices/changed/indi.lan",
+		}
+	}).then(function(msg) {
+		var data = [{
+			key: 'Events',
+			values: [],
+		}];
+		var byAction = {}
+		angular.forEach(msg.p.events, function(event) {
+			var x = (new Date(event.time)).getTime();
+			if (x < 0) {
+				return;
+			}
+			if (!byAction[event.action]) {
+				byAction[event.action] = {
+					key: event.action,
+					values: [],
+				};
+			}
+			byAction[event.action].values.push({
+				x: x,
+				y: event.value,
+			});
+		})
+		var data = []
+		for (action in byAction) {
+			data.push(byAction[action]);
+		}
+		console.log(msg, byAction, data);
+
+		var tickMultiFormat = d3.time.format.multi([
+			["%b %-d, %0H:%M", function(d) { return d.getMinutes(); }], // not the beginning of the hour
+			["%b %-d, %-H:%M",    function(d) { return d.getHours(); }], // not midnight
+			["%b %-d",   function(d) { return d.getDate() != 1; }], // not the first of the month
+			["%b %-d",   function(d) { return d.getMonth(); }], // not Jan 1st
+			["%Y",       function() { return true; }]
+		]);
+
+		nv.addGraph(function() {
+			var chart = nv.models.scatterChart()
+			.showDistX(true)
+			.showDistY(true)
+			.color(d3.scale.category10().range());
+
+			chart.tooltipContent(function(key, x, y, graph) {
+				return '<h3>' + key + '</h3>';
+			});
+
+			chart.xAxis
+				.showMaxMin(false)
+				//.rotateLabels(45)
+				.tickPadding(10)
+				.tickFormat(function(d) { return tickMultiFormat(new Date(d)); });
+			chart.xScale(d3.time.scale());
+			chart.yAxis.tickFormat(d3.format('.02f'));
+
+			d3.select('#chart svg')
+			.datum(data)
+			.call(chart);
+
+			nv.utils.windowResize(chart.update);
+
+			return chart;
+		});
+	});
 });
